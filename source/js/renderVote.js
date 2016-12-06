d00dd41fa224a43128ddf51d493bf99257dfd7f7
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-12-06 18:00:21 -0500

    subiendo express

---
+++ source/js/renderVote.js
@@ -0,0 +1,41 @@
+function renderVote(state) {
+	if (!state.vote) {
+		var mount = $('#options-mount');
+
+		if (mount.length) {
+			mount.append('<div>lo sentimos no hay votación activa</div>');
+		 }
+
+		 return null;
+	}
+
+	var pair = state.vote.pair;
+	var options = '';
+
+	var votedEntry = state.hasVoted;
+
+	var disabled = votedEntry
+		? 'disabled'
+		: '';
+
+	pair.forEach(function (ele) {
+		options += (
+			'<button type="button" class="opciones js-option" data-option="' + ele +'" ' + disabled + '>' +
+				ele +
+			'</button>'
+		);
+	});
+
+	if (votedEntry) {
+		options += '<div>Has votado por: ' + votedEntry + '</div>';
+	}
+
+	var mount = $('#options-mount');
+
+	if (mount.length) {
+		mount.empty();
+		mount.append(options);
+	}
+};
+
+module.exports = renderVote;
---
+++ source/js/start.js
@@ -0,0 +1,33 @@
+var redux = require('redux');
+var createLogger = require('redux-logger');
+var reducer = require('./reducer');
+var socket = require('socket.io-client')('http://localhost:8080');
+//var socket = require('socket.io-client')('https://limitless-lake-44106.herokuapp.com/');
+
+var remoteActionsMiddleware = require('./middleware');
+
+var logger = createLogger();
+
+var store = redux.createStore(reducer, redux.applyMiddleware(logger, remoteActionsMiddleware(socket)));
+
+var renderVote = require('./renderVote');
+var renderResults = require('./renderResults');
+
+store.subscribe(function () {
+	renderVote(store.getState());
+	renderResults(store.getState());
+});
+
+function listen() {
+	socket.on('state', function(state){
+		store.dispatch({
+			type:'SET_STATE',
+			state : state
+		});
+	});
+}
+
+module.exports = {
+	listen:listen,
+	store:store
+};
